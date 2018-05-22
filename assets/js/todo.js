ae92b2ca916cbd73f77ff012de9a15aacb8d2151
Aylin DeBruyne <adebruyne88@gmail.com>
2018-05-22 17:14:08 -0400

    add event handler for input

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -5,14 +5,26 @@
 // })
 //could also write this shorter like this:
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){  //when an li is clicked inside of the UL     .on adding events to current and future elements
     $(this).toggleClass("completed");
 });
 
 //*************Click on X to delete */
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){       
         $(this).remove();
     });
     event.stopPropagation(); //keeps the function from bubbling up to parent elements 
+})
+
+//********** handling the text input*/
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing new todo text from input
+       var todoText = $(this).val();
+       //clear the input box
+       $(this).val("");
+       //create a new li and add to ul
+       $("ul").append("<li><span>X</span> " + todoText + "</li>");   //takes a string of html and appends those elements to whatever we selected
+    }
 })
